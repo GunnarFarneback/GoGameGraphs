0fb9e2dd3ca36066bdba25106bb90c58bf8a41ab
Gunnar FarnebÃ¤ck <gunnar@lysator.liu.se>
2018-08-12 15:39:31 +0200

    Add Julia tests.

---
+++ test/runtests.jl
@@ -0,0 +1,63 @@
+using GoGameGraphs
+using Base.Test
+
+for id in reduce(vcat, [[2^i-1, 2^i] for i in 1:62])
+    @test GoGameGraphs.num_nodes(id) == length(Board(id).edges)
+end
+
+filename = tempname()
+for id in [1, 3, 7, 11, 12, 13, 15, 30, 31, 63]
+    export_graph(filename, GameGraph(Board(id)))
+    s = readstring(joinpath(dirname(@__FILE__), "../graphs/graph$(id)"))
+    @test s == readstring(filename)
+end
+
+function is_path_valid(graph::GameGraph, path::Vector{Int})
+    for i = 2:length(path)
+        if path[i] ∉ graph.edges[path[i - 1]]
+            return false
+        end
+    end
+    return true
+end
+
+sample_paths_dir = joinpath(dirname(@__FILE__), "../sample_longest_paths")
+function read_sample_path(filename)
+    path_string = readstring(joinpath(sample_paths_dir, filename))
+    path = [parse(Int, n) for n in split(path_string, "\n", keep = false)]
+    return path
+end
+
+for filename in readdir(sample_paths_dir)
+    path = read_sample_path(filename)
+    graph = GameGraph(Board(parse(Int, replace(filename, "path", ""))))
+    @test is_path_valid(graph, path)
+end
+
+for (num_nodes, num_unique_graphs) in [(2, 2),
+                                       (3, 4),
+                                       (4, 11),
+                                       (5, 34),
+                                       (6, 156),
+                                       (7, 1044)]
+    @test length(unique_graphs(num_nodes)) == num_unique_graphs
+end
+
+for id1 = 1:500:100000
+    board1 = Board(id1)
+    graph1 = GameGraph(board1)
+    id2 = smallest_isomorphic_id(board1)
+    graph2 = GameGraph(Board(id2))
+    @test length(graph1.edges) == length(graph2.edges)
+    @test sum(length.(graph1.edges)) == sum(length.(graph2.edges))
+    N = length(board1.edges)
+    @test is_board_graph_connected(id1, N) == is_board_graph_connected(id2, N)
+end
+
+source_board = Board(3448)
+source_path = read_sample_path("path3448")
+target_board = Board([[2,4],[1,3,5],[2,6],[1,5],[2,4,6],[3,5]])
+@test smallest_isomorphic_id(target_board) == 3448
+M = remap(source_board, target_board)
+target_path = M[source_path]
+@test is_path_valid(GameGraph(target_board), target_path)
